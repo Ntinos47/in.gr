8e91b3307efcf08f2a16a998ac47099f8a6a63ef
ntinos <120028619+Ntinos47@users.noreply.github.com>
2024-05-22 13:07:11 +0300

    update-Needs work

--- js/index.js
+++ js/index.js
@@ -57,7 +57,54 @@ document.getElementById('text2').addEventListener('click', function() {
 
 
 document.getElementById('switch2').addEventListener('click', function() {
-    col1.style.width = '18%';
-    col2.style.width = '82%';
+    col1.style.width = '30%';
+    col2.style.width = '70%';
     col3.style.width = '0';
 });
+
+
+function toggleContent1() {
+
+    var div = document.getElementById("col1");
+    var newDiv = document.getElementById("colReplace1");
+
+
+    // Check if myDiv currently displays original content
+    if (div.innerHTML === div) {
+        // Change the content of myDiv to newContent's content
+        div.innerHTML = newDiv.innerHTML;
+    } else {
+        // Change the content of myDiv back to original content
+        div.innerHTML = newDiv;
+    }
+}
+function toggleContent2() {
+
+    var div = document.getElementById("col2");
+    var newDiv = document.getElementById("colReplace2");
+
+
+    // Check if myDiv currently displays original content
+    if (div.innerHTML === div) {
+        // Change the content of myDiv to newContent's content
+        div.innerHTML = newDiv.innerHTML;
+    } else {
+        // Change the content of myDiv back to original content
+        div.innerHTML = newDiv;
+    }
+}
+function toggleContent3() {
+
+    var div = document.getElementById("col3");
+    var newDiv = document.getElementById("colReplace3");
+
+
+    // Check if myDiv currently displays original content
+    if (div.innerHTML === div) {
+        // Change the content of myDiv to newContent's content
+        div.innerHTML = newDiv.innerHTML;
+    } else {
+        // Change the content of myDiv back to original content
+        div.innerHTML = newDiv;
+    }
+}
