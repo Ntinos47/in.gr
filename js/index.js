edd8a03d0509ef530e8f8ee21f807cd5c70cdc3c
SSAKIS47 <skiadaskonstantinos8@gmail.com>
2024-05-31 11:09:18 +0300

    Update
    All slider fixed, small problem with the 2nd

--- js/index.js
+++ js/index.js
@@ -79,39 +79,33 @@ document.getElementById('switch2').addEventListener('click', function() {
 function toggleContent(counter) {
 
     var div1 = document.getElementById("col1");
-    var newDiv1 = document.getElementById("colReplace1");
     var div2 = document.getElementById("col2");
-    var newDiv2 = document.getElementById("colReplace2");
     var div3 = document.getElementById("col3");
+    var newDiv1 = document.getElementById("colReplace1");
+    var newDiv2 = document.getElementById("colReplace2");
     var newDiv3 = document.getElementById("colReplace3");
 
     if (counter == 2) {
-        // Store original content temporarily
         var temp1 = div1.innerHTML;
         var temp2 = div2.innerHTML;
         var temp3 = div3.innerHTML;
 
-        // Replace with new content
         div1.innerHTML = newDiv1.innerHTML;
         div2.innerHTML = newDiv2.innerHTML;
         div3.innerHTML = newDiv3.innerHTML;
 
-        // Update replacements with the original content
         newDiv1.innerHTML = temp1;
         newDiv2.innerHTML = temp2;
         newDiv3.innerHTML = temp3;
     } else if (counter == 1) {
-        // Store swapped content temporarily
         var temp1 = newDiv1.innerHTML;
         var temp2 = newDiv2.innerHTML;
         var temp3 = newDiv3.innerHTML;
 
-        // Replace with original content
         newDiv1.innerHTML = div1.innerHTML;
         newDiv2.innerHTML = div2.innerHTML;
         newDiv3.innerHTML = div3.innerHTML;
 
-        // Update original divs with swapped content
         div1.innerHTML = temp1;
         div2.innerHTML = temp2;
         div3.innerHTML = temp3;
@@ -126,7 +120,65 @@ document.getElementById('text2').addEventListener('click', function() {
     section3.style.transform = 'translateX(0)';
     wrapimgfooter2.style.zIndex = '0';
     wraplogo2.style.zIndex = '0';
-    switch3.style.left = '17%';
-    switch3.style.transform = 'translate(45%)';
 });
 
+
+var count = 1;
+document.getElementById('switch3').addEventListener('click', function() {
+    if (count == 1) {
+        colu1.style.maxWidth = '34%';
+        colu2.style.width = '48%';
+        colu3.style.width = '18%';
+        switch3.style.right = '17%';
+        switch3.style.transform = 'translate(30%)';
+
+        count = 2;
+        toggleContent1(count);
+
+    } else if (count == 2) {
+        colu1.style.width = '24%';
+        colu2.style.width = '76%';
+        colu3.style.width = '0';
+        switch3.style.right = '77%';
+        switch3.style.transform = 'translate(40%)';
+
+        count = 1;
+        toggleContent1(count);
+    }
+});
+
+function toggleContent1(count) {
+
+    var Div1 = document.getElementById("col1-2");
+    var Div2 = document.getElementById("col2-2");
+    var Div3 = document.getElementById("col3-2");
+    var newDiv1 = document.getElementById("col-1");
+    var newDiv2 = document.getElementById("col-2");
+    var newDiv3 = document.getElementById("col-3");
+
+    if (count == 2) {
+        var temp1 = Div1.innerHTML;
+        var temp2 = Div2.innerHTML;
+        var temp3 = Div3.innerHTML;
+
+        Div1.innerHTML = newDiv1.innerHTML;
+        Div2.innerHTML = newDiv2.innerHTML;
+        Div3.innerHTML = newDiv3.innerHTML;
+
+        newDiv1.innerHTML = temp1;
+        newDiv2.innerHTML = temp2;
+        newDiv3.innerHTML = temp3;
+    } else if (count == 1) {
+        var temp1 = newDiv1.innerHTML;
+        var temp2 = newDiv2.innerHTML;
+        var temp3 = newDiv3.innerHTML;
+
+        newDiv1.innerHTML = Div1.innerHTML;
+        newDiv2.innerHTML = Div2.innerHTML;
+        newDiv3.innerHTML = Div3.innerHTML;
+
+        Div1.innerHTML = temp1;
+        Div2.innerHTML = temp2;
+        Div3.innerHTML = temp3;
+    }
+}
